1bc37d25cd636179ca75ee4e03edc90ff3b1ed6e
moazedy <moazedy@gmail.com>
2024-12-08 11:18:37 +0330

    feature: implementing needed casts for todo items core services

---
+++ internal/domain/srvimplement/cast/todo_item.go
@@ -0,0 +1,38 @@
+package cast
+
+import (
+	"github.com/moazedy/todo/internal/domain/model"
+	"github.com/moazedy/todo/internal/port/driver/service/dto"
+)
+
+func CreateTodoItemRequestToServiceModel(in dto.CreateTodoItemRequest) model.TodoItem {
+	return model.TodoItem{
+		Description: in.Description,
+		DueDate:     in.DueDate,
+		FileID:      in.FileID,
+	}
+}
+
+func ToCreateTodoItemResponse(in model.TodoItem) dto.CreateTodoItemResponse {
+	return dto.CreateTodoItemResponse{
+		ID: in.ID,
+	}
+}
+
+func UpdateTodoItemRequestToServiceModel(in dto.UpdateTodoItemRequest) model.TodoItem {
+	return model.TodoItem{
+		ID:          in.ID,
+		Description: in.Description,
+		DueDate:     in.DueDate,
+		FileID:      in.FileID,
+	}
+}
+
+func ToGetTodoItemByIDResponse(in model.TodoItem) dto.GetTodoItemByIDResponse {
+	return dto.GetTodoItemByIDResponse{
+		ID:          in.ID,
+		Description: in.Description,
+		DueDate:     in.DueDate,
+		FileID:      in.FileID,
+	}
+}

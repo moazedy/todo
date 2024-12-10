ffb4839a2a4fb308a1a5685764aa8f0448bc16f1
moazedy <moazedy@gmail.com>
2024-12-10 18:45:19 +0330

    feature: defining needed routes for file management APIs

--- internal/adapter/driver/httpimplement/routes.go
+++ internal/adapter/driver/httpimplement/routes.go
@@ -11,6 +11,7 @@ import (
 	"github.com/moazedy/todo/internal/port/driver/http"
 	"github.com/moazedy/todo/internal/port/driver/service"
 	"github.com/moazedy/todo/pkg/infra/config"
+	"github.com/moazedy/todo/pkg/infra/storage"
 	"github.com/moazedy/todo/pkg/infra/tx"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -20,15 +21,19 @@ type serverItems struct {
 	// basics
 	dbConnection *gorm.DB
 	txFactory    tx.TXFactory
+	storageAgent storage.StorageAgent
 
 	// repo layer
 	todoItemRepoFactory repository.GenericRepoFactory[model.TodoItem]
+	fileRepo            repository.File
 
 	// service layer
 	todoService service.TodoItem
+	fileService service.File
 
 	// presentation layer
 	todoController http.TodoItem
+	fileController http.File
 }
 
 var items serverItems
@@ -36,12 +41,20 @@ var items serverItems
 func register(app *echo.Echo, cfg config.Config) {
 	items.dbConnection = initDB(cfg.Postgres)
 	items.txFactory = tx.NewTXFactory(items.dbConnection)
+	items.storageAgent = storage.NewStorageAgent(nil, cfg.Storage.Bucket)
 
 	items.todoItemRepoFactory = repoimplement.NewGenericRepoFactory[model.TodoItem]()
+	items.fileRepo = repoimplement.NewFile(items.storageAgent)
 
 	items.todoService = srvimplement.NewTodoItem(items.txFactory, items.todoItemRepoFactory)
+	items.fileService = srvimplement.NewFile(items.fileRepo)
 
 	items.todoController = NewTodoItem(items.todoService)
+
+	items.fileController = NewFile(items.fileService)
+
+	app.POST("/file", items.fileController.Upload)
+	app.GET("/file/:file_id", items.fileController.Download)
 
 	app.POST("/todo/item", items.todoController.Create)
 	app.PUT("/todo/item", items.todoController.Update)

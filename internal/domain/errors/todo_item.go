52636b2085287d304f85313f36fe592292f60a74
moazedy <moazedy@gmail.com>
2024-12-11 18:25:04 +0330

    feature: adding new needed errors related to file analyzing process

--- internal/domain/errors/todo_item.go
+++ internal/domain/errors/todo_item.go
@@ -3,3 +3,7 @@ package errors
 import "github.com/moazedy/todo/pkg/cerror"
 
 var ErrTodoItemNotFound = cerror.NewNotFoundError("requested todo item not found")
+
+var ErrFileTypeNotAllowed = cerror.NewForbiddenError("file type not allowed")
+
+var ErrFileIsOverSized = cerror.NewForbiddenError("file is over sized")

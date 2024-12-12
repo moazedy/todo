956a6291d4a383382b43c63a42f5528ad7b6f73c
moazedy <moazedy@gmail.com>
2024-12-12 12:50:15 +0330

    feature : passing needed dependencies for benith layers

--- internal/adapter/driver/httpimplement/routes.go
+++ internal/adapter/driver/httpimplement/routes.go
@@ -3,6 +3,7 @@ package httpimplement
 import (
 	"fmt"
 
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -15,6 +16,7 @@ import (
 	"github.com/moazedy/todo/internal/port/driver/http"
 	"github.com/moazedy/todo/internal/port/driver/service"
 	"github.com/moazedy/todo/pkg/infra/config"
+	"github.com/moazedy/todo/pkg/infra/queue"
 	"github.com/moazedy/todo/pkg/infra/storage"
 	"github.com/moazedy/todo/pkg/infra/tx"
 	"gorm.io/driver/postgres"
@@ -23,14 +25,17 @@ import (
 
 type serverItems struct {
 	// basics
-	dbConnection *gorm.DB
-	txFactory    tx.TXFactory
-	storageAgent storage.StorageAgent
-	awsClient    *s3.S3
+	dbConnection     *gorm.DB
+	txFactory        tx.TXFactory
+	storageAgent     storage.StorageAgent
+	awsClient        *s3.S3
+	sqsClient        *sqs.Client
+	sqsClientFactory queue.SQSClientFactory
 
 	// repo layer
 	todoItemRepoFactory repository.GenericRepoFactory[model.TodoItem]
 	fileRepo            repository.File
+	queueRepo           repository.Queue
 
 	// service layer
 	todoService service.TodoItem
@@ -48,11 +53,14 @@ func register(app *echo.Echo, cfg config.Config) {
 	items.txFactory = tx.NewTXFactory(items.dbConnection)
 	items.awsClient = createAWSS3Client(cfg.Storage.Endpoint, cfg.Storage.AccessKey, cfg.Storage.SecretKey, cfg.Storage.Bucket)
 	items.storageAgent = storage.NewStorageAgent(items.awsClient, cfg.Storage.Bucket)
+	items.sqsClient = queue.NewSQSClient(cfg.Queue)
+	items.sqsClientFactory = queue.NewSQSClientFactory(cfg.Queue.IsMock, items.sqsClient)
 
 	items.todoItemRepoFactory = repoimplement.NewGenericRepoFactory[model.TodoItem]()
 	items.fileRepo = repoimplement.NewFile(items.storageAgent)
+	items.queueRepo = repoimplement.NewQueue(items.sqsClientFactory, cfg.Queue.QueueUrl)
 
-	items.todoService = srvimplement.NewTodoItem(items.txFactory, items.todoItemRepoFactory)
+	items.todoService = srvimplement.NewTodoItem(items.txFactory, items.todoItemRepoFactory, items.queueRepo)
 	items.fileService = srvimplement.NewFile(items.fileRepo, cfg.Storage.MaxFileSize)
 
 	items.todoController = NewTodoItem(items.todoService)

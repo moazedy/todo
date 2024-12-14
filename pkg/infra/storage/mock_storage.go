e7b99f6cc3199fc59ecb11a619275f13f2b78bd1
moazedy <moazedy@gmail.com>
2024-12-14 13:37:09 +0330

    feature: adding code snipet for handling download on unit test cases

--- pkg/infra/storage/mock_storage.go
+++ pkg/infra/storage/mock_storage.go
@@ -21,6 +21,10 @@ func (s *mockStorageAgent) UploadFile(ctx context.Context, file []byte, filename
 }
 
 func (s *mockStorageAgent) DownloadFile(ctx context.Context, filename string) ([]byte, error) {
+	if filename == "testfilename" {
+		return []byte("this is the test file"), nil
+	}
+
 	file, exist := s.repo[filename]
 	if !exist {
 		return nil, errors.New("file not found")
